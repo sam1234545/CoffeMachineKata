b59548e18abd2ce4d4a6595a6423b269b19cced6
Samah NABIH <samahnabih@gmail.fr>
2023-10-09 09:29:34 +0200

    Make coffee test green

--- CoffeMachineKata/CoffeMaker.cs
+++ CoffeMachineKata/CoffeMaker.cs
@@ -5,6 +5,10 @@
 
         public string MakeDrink(string order)
         {
+            if (order.StartsWith('C'))
+            {
+                return "Coffee with 1 sugar and 1 spoon";
+            }
             return "Tea with 1 sugar and 1 spoon";
         }
     }
--- CoffeMachineKata/CoffeMakerTest.cs
+++ CoffeMachineKata/CoffeMakerTest.cs
@@ -9,5 +9,13 @@ namespace CoffeMachineKata
             var order = CoffeMaker.MakeDrink("T:1:1");
             Assert.Equal("Tea with 1 sugar and 1 spoon", order);
         }
+
+        [Fact]
+        public void Should_SendOneCoffee_OneSugar_OneSpoon_WhenOrdered()
+        {
+            var CoffeMaker = new CoffeMaker();
+            var order = CoffeMaker.MakeDrink("C:1:1");
+            Assert.Equal("Coffee with 1 sugar and 1 spoon", order);
+        }
     }
 }
