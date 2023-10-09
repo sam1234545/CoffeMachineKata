047c9e2654dd4dc942345e2d0f0b524007034d2f
Samah NABIH <samahnabih@gmail.fr>
2023-10-09 09:27:01 +0200

    Extract Class and Method from Test

---
+++ CoffeMachineKata/CoffeMaker.cs
@@ -0,0 +1,11 @@
+﻿namespace CoffeMachineKata
+{
+    public class CoffeMaker
+    {
+
+        public string MakeDrink(string order)
+        {
+            return "Tea with 1 sugar and 1 spoon";
+        }
+    }
+}
--- CoffeMachineKata/CoffeMakerTest.cs
+++ CoffeMachineKata/CoffeMakerTest.cs
@@ -1,17 +1,13 @@
 namespace CoffeMachineKata
 {
-    public class CoffeMakerTest
+    public class CoffeMakerTest 
     {
         [Fact]
         public void Should_SendOneTea_OneSugar_OneSpoon_WhenOrdered()
         {
-            var order = MakeDrink("T:1:1");
+            var CoffeMaker = new CoffeMaker();
+            var order = CoffeMaker.MakeDrink("T:1:1");
             Assert.Equal("Tea with 1 sugar and 1 spoon", order);
-        }
-
-        private string MakeDrink(string order)
-        {
-            return "Tea with 1 sugar and 1 spoon";
         }
     }
 }
