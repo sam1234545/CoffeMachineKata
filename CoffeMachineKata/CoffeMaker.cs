5704f6fb6a78172c8c156864942808a22c24429d
Samah NABIH <samahnabih@gmail.fr>
2023-10-09 10:36:28 +0200

    Extract SetOrder Method

--- CoffeMachineKata/CoffeMaker.cs
+++ CoffeMachineKata/CoffeMaker.cs
@@ -8,23 +8,31 @@ namespace CoffeMachineKata
         const string COFFEE = "C";
         const string TEA = "T";
 
+        string? _drink;
+        string? _sugar;
+        string? _spoon;
+
         public string MakeDrink(string order)
         {
-            
-            string[] orderInstructions = order.Split(':');
-            var drink = orderInstructions[0];
-            var sugar = orderInstructions[1];
-            var spoon = orderInstructions[1] != "0" ? orderInstructions[2]:"0";
+            SetOrder(order);
 
-            if (drink ==CHOCOLATE)
+            if (_drink == CHOCOLATE)
             {
                 return "Chocolate with 1 sugar and 1 spoon";
             }
-            if (drink == COFFEE)
+            if (_drink == COFFEE)
             {
                 return "Coffee with 1 sugar and 1 spoon";
             }
-            return string.Format("Tea with {0} sugar and {1} spoon",sugar,spoon);
+            return string.Format("Tea with {0} sugar and {1} spoon", _sugar, _spoon);
+        }
+
+        private void SetOrder(string order)
+        {
+            string[] orderInstructions = order.Split(':');
+            _drink = orderInstructions[0];
+            _sugar = orderInstructions[1];
+            _spoon = orderInstructions[1] != "0" ? orderInstructions[2] : "0";
         }
     }
 }
