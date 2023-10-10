251b9261e43fc9d91c10217d3ac77f6289e651cf
Samah NABIH <samahnabih@gmail.fr>
2023-10-10 09:20:36 +0200

    Delete DrinkOrder interface

--- CoffeMachineKata/IDrinkOrder.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CoffeMachineKata
-{
-    internal interface IDrinkOrder
-    {
-        void SetDrinkOrder(string order);
-        void SetSpoon(string[] orderInstructions);
-        void SetSugar(string[] orderInstructions);
-    }
-}
